8f7c594fb67e847531e18706c2098a3125ddd653
reym-sys <reyzanmhafizh06@gmail.com>
2024-10-28 14:01:14 +0700

    FEAT[Menambahkan sidebar & navbar]

---
+++ resources/views/home.blade.php
@@ -0,0 +1,13 @@
+@extends('layouts.index')
+
+@section('content')
+    <div class="about-us container">
+        <h1>Tentang Saya</h1>
+        <p>Selamat datang di website saya! Di sini, saya hadir untuk memberikan konten dan layanan yang keren dan bermanfaat
+            banget buat kamu. Tujuan saya adalah berbagi wawasan dan solusi yang berkualitas melalui pengalaman dan dedikasi
+            saya.</p>
+        <p>Saya berkomitmen untuk menyajikan pengalaman dan solusi yang lebih dari sekadar biasa. Baik kamu di sini untuk
+            mengeksplor apa yang saya tawarkan atau sekadar belajar lebih banyak tentang layanan saya, saya senang banget
+            kamu mampir!</p>
+    </div>
+@endsection
--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -20,7 +20,7 @@
         </div>
     </div>
     {{-- end content --}}
-    <script src="{{ asset('/') }}assets/js/bootstrap.bundle.min.js"
+    <script src="{{ asset('/') }}assets/js/bootstrap.bundle.min.js" 
         integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
     </script>
 
