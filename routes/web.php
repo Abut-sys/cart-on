3d801956e1988504d39b7ffc77cbf02f6d85d554
sepatch-sys <syafaatfebriansyah2@gmail.com>
2024-10-30 14:28:25 +0700

    FIX[Auth]

--- routes/web.php
+++ routes/web.php
@@ -22,9 +22,14 @@ use App\Http\Controllers\VoucherController;
 */
 
 
-//rute brand
 
+<<<<<<< HEAD
 
+=======
+Route::resource('brands', BrandController::class);
+Route::resource('products', ProductController::class);
+Route::resource('categories', CategoryProductController::class);
+>>>>>>> 9f2868d00f64acae1a3ff514f5179a7be364090c
 
 
 
@@ -33,17 +38,13 @@ Route::middleware(['auth'])->group(function () {
         return view('home');
     });
 
-Route::get('login', [LoginController::class, 'create'])->name('login');
-Route::post('login', [LoginController::class, 'store']);
+    Route::get('/home', function () {
+        return view('home');
+    });
 
-Route::get('register', [RegisterController::class, 'create'])->name('register');
-Route::post('register', [RegisterController::class, 'store']);
+    Route::resource('brands', BrandController::class);
 
-//rute brand
-
-Route::resource('brands', BrandController::class);
-
-Route::resource('categories', CategoryProductController::class);
+    Route::resource('categories', CategoryProductController::class);
 
     Route::resource('vouchers', VoucherController::class);
 
