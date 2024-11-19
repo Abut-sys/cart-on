621f2836628dc4e19acac0c8c5dac084682c0cce
ariputraa <aripoetra5858@gmail.com>
2024-11-19 14:14:25 +0700

    FEAT[PROD TO MANY SUB] update soon

--- app/Models/product.php
+++ app/Models/product.php
@@ -6,10 +6,10 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class product extends Model
-
 {
     use HasFactory;
 
+    // Allow mass assignment for these attributes
     protected $fillable = [
         'name',
         'brands_id',
@@ -18,27 +18,39 @@ class product extends Model
         'description',
         'price',
         'stock',
-        'image_path'
+        'image_path',
     ];
 
+    /**
+     * Relationship with Brand model
+     */
     public function brand()
     {
         return $this->belongsTo(Brand::class, 'brands_id');
     }
 
+    /**
+     * Relationship with SubVariant model
+     */
     public function subVariants()
     {
         return $this->hasMany(SubVariant::class);
     }
 
+    /**
+     * Relationship with CategoryProduct model
+     */
     public function categoryProduct()
     {
         return $this->belongsTo(CategoryProduct::class, 'category_products_id');
     }
 
-    public function subCategoryProduct()
+    /**
+     * Many-to-Many relationship with SubCategoryProduct model via 'product_sub_category' pivot table
+     */
+    public function subCategoryProducts()
     {
-        return $this->belongsTo(SubCategoryProduct::class, 'sub_category_products_id');
+        return $this->belongsToMany(SubCategoryProduct::class, 'product_sub_category', 'products_id', 'sub_category_products_id');
     }
 
 }
